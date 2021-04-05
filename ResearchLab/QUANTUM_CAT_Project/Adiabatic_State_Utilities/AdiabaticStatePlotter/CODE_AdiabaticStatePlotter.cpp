71caa87d3f423a01f06e8aa57e8f3d2737592524
Xabier_Oyanguren_Asua <oiangu9@gmail.com>
2021-04-05 11:08:26 +0200

    Updated Report

--- ResearchLab/QUANTUM_CAT_Project/Adiabatic_State_Utilities/AdiabaticStatePlotter/CODE_AdiabaticStatePlotter.cpp
+++ ResearchLab/QUANTUM_CAT_Project/Adiabatic_State_Utilities/AdiabaticStatePlotter/CODE_AdiabaticStatePlotter.cpp
@@ -14,10 +14,10 @@ using namespace Eigen;
 using namespace std;
 #define PI 3.141592653589793238463
 #define INF 1000000.0
-int nx=250, ny=150, jmax=10;
-double xmin=-20.0, xmax=15.0, ymin=-20.0, ymax=20.0, dx, dy;
-cdouble EigenstatesForSectionsInx(double y, double x, int j){ double g=1.0,a1=0.0,a2=12.0,Lmax=20.0,Lmin=7.0,o, L; o= -(Lmax-Lmin)/2.0*(1.0/(1.0+exp((x-a1)/g))+1.0/(1.0+exp((-x+a2)/g)))-Lmin/2.0; L=-2*o; if(y>(o+L) || y<o){return 0.0;} else{return sqrt(2.0/L)*sin(j*PI*(y-o)/L);}}
-double potential(double x, double y){double sigmax=2.0, sigmay=3, mux=-10.0, muy=0.0, kx=0.0, ky=0; return pow(1.0/(2*PI*sigmax*sigmax),0.25)*exp(-0.25*pow((x-mux)/sigmax,2))* pow(1.0/(2*PI*sigmay*sigmay),0.25)*exp(-0.25*pow((y-muy)/sigmay,2));}
+int nx=500, ny=700, jmax=10;
+double xmin=-50.0, xmax=70.0, ymin=-20.0, ymax=20.0, dx, dy;
+cdouble EigenstatesForSectionsInx(double y, double x, int j){ double g=3.0,a1=5.0,a2=40.0,Lmax=40.0,Lmin=5.0,o, L; o= -(Lmax-Lmin)/2.0*(1.0/(1.0+exp((x-a1)/g))+1.0/(1.0+exp((-x+a2)/g)))-Lmin/2.0; L=-2*o; if(y>(o+L) || y<o){return 0.0;} else{return sqrt(2.0/L)*sin(j*PI*(y-o)/L);}}
+double potential(double x, double y){double g=3.0,a1=5.0,a2=40.0,Lmax=40.0,Lmin=5.0,L1; L1= (Lmax-Lmin)/2.0*(1.0/(1.0+exp((x-a1)/g))+1.0/(1.0+exp((-x+a2)/g)))+Lmin/2.0; if(y>L1 || y<-L1){return 100.0;} else{return 0.0;}}
 ArrayXd posx(nx+1), posy(ny+1);
 int main(){
 ofstream plotFile, potentialFile;
